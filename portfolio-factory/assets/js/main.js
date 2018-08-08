925d5e87dba8b7da9f91fd981e91a2beab4df02e
Monowar Hossain <32286368+monowar-hossain@users.noreply.github.com>
2018-08-08 09:39:23 +0600

    Add files via upload

---
+++ portfolio-factory/assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.slider-area').owlCarousel({
+    loop:true,
+	items:1,
+    nav:false,
+	dots:true
+});
+  $('.logo-carousel').owlCarousel({
+    loop:true,
+	items:5,
+	margin:30,
+    nav:false,
+	dots:true
+})
+});
